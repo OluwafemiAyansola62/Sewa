b763c51099b91eedd89fc8538cb462d77de3127b
OluwafemiAyansola62 <oluwafemiayansola62@gmail.com>
2022-09-19 16:15:53 +0100

    asdfgh

---
+++ testing.c
@@ -0,0 +1,25 @@
+This readme is for good testing of stuffs
+sewa is now a software engineer
+Uncle femi is a fullstack Software engineer
+
+#include <stdio.h>
+
+/**
+* main - prints numbers 1 to 9
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for(i = 1; i < 10; i++)
+{
+putchar(i);
+putchar(,);
+putchar();
+}
+return (0);
+}
+
